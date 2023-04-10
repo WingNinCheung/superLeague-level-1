5cf1313d03810b822a32680faedfc9849793e36c
Wing Nin Cheung <wingnin.cheung415@gmail.com>
2023-04-10 15:06:56 -0700

    completed style

--- src/App.js
+++ src/App.js
@@ -54,28 +54,55 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <div>Click Count: {clickCount}</div>
-      <button onClick={handleClick}>Click</button>
-      <table>
+    <div className="main">
+      <h1>Super League Level 1 Challenge</h1>
+      <div className="click-main">
+        <div id="count-div">Click Count: {clickCount}</div>
+        <button id="click-btn" onClick={handleClick}>
+          Click
+        </button>
+      </div>
+      <table className="table-main">
         <thead>
           <tr>
-            <th>Distribution of Clicks by States</th>
+            <th id="table-title">Distribution of Click by State</th>
           </tr>
         </thead>
-        <tbody>
-          <tr>
-            <td>State</td>
+        <tbody className="table-body">
+          <tr className="label">
+            <td className="state">State</td>
             <td>Count</td>
           </tr>
           {Object.keys(stateClick).map((state, i) => (
-            <tr key={i}>
+            <tr key={i} className="label">
               <td>{state}</td>
               <td>{stateClick[state]}</td>
             </tr>
           ))}
         </tbody>
       </table>
+      <div className="footer">
+        <img
+          className="pic"
+          alt="my-pic"
+          src="https://avatars.githubusercontent.com/u/96600317?v=4"
+        ></img>
+        <div>
+          <div className="name">Ricky Cheung</div>
+          <a href="https://www.linkedin.com/in/wingnincheung/" target="popup">
+            <i class="fa-brands fa-linkedin"></i>
+          </a>
+          <a
+            href="https://github.com/WingNinCheung/superLeague-level-1"
+            target="popup"
+          >
+            <i class="fa-brands fa-github"></i>
+          </a>
+          <a href="mailto:rickycheung.dev@gmail.com" className="a-links">
+            <i class="fa-solid fa-envelope"></i>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
