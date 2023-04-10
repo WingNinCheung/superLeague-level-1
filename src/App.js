a6dfb76e34b53ac24acd1b20e2634ea529dbeb61
Wing Nin Cheung <wingnin.cheung415@gmail.com>
2023-04-10 11:00:43 -0700

    finished basic click count feature upon refresh

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  // initialize the clickCount state
+  const [clickCount, setClickCount] = useState(0);
+
+  // handle click event and update clickCount by 1 and set it in local storage
+  const handleClick = (e) => {
+    e.preventDefault();
+    const newCount = clickCount + 1;
+    setClickCount(newCount);
+    localStorage.setItem("count", newCount);
+  };
+
+  // get the previous clickCount from local storage upon refresh
+  useEffect(() => {
+    setClickCount(parseInt(localStorage.getItem("count")) || 0);
+  }, []);
+
   return (
-    <h1>Hello from App</h1>
+    <div>
+      <div>Click Count: {clickCount}</div>
+      <button onClick={handleClick}>Click</button>
+    </div>
   );
 }
 
